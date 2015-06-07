969c1433abf9bbbe5c83af734a07f8c8c0fbc84b
CronGM <cron@outlook.com>
2015-06-07 02:21:03 -0500

    Implemented ng-repeat for card slots.

--- scripts/controllers/main-controller.js
+++ scripts/controllers/main-controller.js
@@ -4,4 +4,7 @@ app.controller('MainController', ['$scope', function ($scope) {
 	var vm = this;
 
 	vm.testvar = 'hola goey';
+	vm.getNumber = function(num) {
+        return new Array(num);   
+    }
 }]);
