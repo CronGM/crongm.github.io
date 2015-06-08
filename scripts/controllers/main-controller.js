4c96a82bb5a290a9ac729703b0bbbb8cbd7fc2d4
CronGM <cron@outlook.com>
2015-06-07 21:57:33 -0500

    Better markup for bingo card.

--- scripts/controllers/main-controller.js
+++ scripts/controllers/main-controller.js
@@ -4,6 +4,15 @@ app.controller('MainController', ['$scope', function ($scope) {
 	var vm = this;
 
 	vm.testvar = 'hola goey';
+
+	vm.card = [
+		{
+			id: 'a',
+			imageUrl: 'http://placevaughnandfranco.it/img/250/250',
+			message: 'Texto de la casilla'
+		}
+	];
+
 	vm.getNumber = function(num) {
         return new Array(num);   
     }
