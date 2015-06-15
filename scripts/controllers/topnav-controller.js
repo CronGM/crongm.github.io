cd559b39d18df18cec63f46771d2680ecb510ab6
CronGM <cron@outlook.com>
2015-06-14 23:55:08 -0500

    Directive to add active class to navbar links.

---
+++ scripts/controllers/topnav-controller.js
@@ -0,0 +1,9 @@
+app.controller('TopNavController', ['$scope', '$location', function ($scope, $location) {
+	vm = this;
+
+	vm.isActive = function (viewLocation) {
+		// Uncomment if you want to take parameters into account
+		// return $location.path().indexOf(viewLocation) == 0;
+		return viewLocation === $location.path();
+	}
+}])
